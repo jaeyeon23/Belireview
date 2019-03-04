a31a9633ffec46f38fe3f3b5a0b9de67a2ca2c3a
woojeongyoon <wjddbs0506@naver.com>
2019-03-04 09:43:17 +0900

    moviecontroller

--- Belireview/src/main/java/believe/review/brw/movie/MovieController.java
+++ Belireview/src/main/java/believe/review/brw/movie/MovieController.java
@@ -1,5 +1,37 @@
 package believe.review.brw.movie;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/Movie")
 public class MovieController {
+	
+	@RequestMapping("moviewList")
+	public ModelAndView movieList() {
+		ModelAndView mv = new ModelAndView("Movie/movieList");
+		return mv;
+	}
+	
+	@RequestMapping("moviewDetail")
+	public ModelAndView movieDetail() {
+		ModelAndView mv = new ModelAndView("Movie/movieDetail");
+		return mv;
+	}
+	
+	@RequestMapping("moviewInfo")
+	public ModelAndView movieInfo() {
+		ModelAndView mv = new ModelAndView("Movie/movieInfo");
+		return mv;
+	}
+	
+	@RequestMapping("movieComment")
+	public ModelAndView movieComment() {
+		ModelAndView mv = new ModelAndView("Movie/movieComment");
+		return mv;
+	}
+	
+	
 
 }
