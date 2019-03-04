7cbf4033de4f260f2253a5127599a88292c314b2
unanabaek <unanabaek@gmail.com>
2019-03-04 17:46:31 +0900

    03-04 end

--- Belireview/src/main/java/believe/review/brw/member/MemberService.java
+++ Belireview/src/main/java/believe/review/brw/member/MemberService.java
@@ -1,5 +1,16 @@
 package believe.review.brw.member;
 
-public interface MemberService {
+//import java.util.List;
+import java.util.Map;
 
+//import javax.servlet.http.HttpServletRequest;
+
+public interface MemberService {
+	void insertMember(Map<String, Object> map) throws Exception;
+	/*
+	 * Map<String, Object> getMember(Map<String, Object> map) throws Exception;
+	 * 
+	 * Map<String, Object> IdCheck(Map<String, Object> map) throws Exception;
+	 * 
+	 */
 }
