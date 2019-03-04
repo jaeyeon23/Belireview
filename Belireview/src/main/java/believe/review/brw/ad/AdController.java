18056148af242b4657b872f539abfd04baec7173
rtg9502 <rtg9502@naver.com>
2019-03-04 09:43:28 +0900

    gg

--- Belireview/src/main/java/believe/review/brw/ad/AdController.java
+++ Belireview/src/main/java/believe/review/brw/ad/AdController.java
@@ -1,5 +1,31 @@
 package believe.review.brw.ad;
 
-public class AdController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping("/ad")
+public class AdController {
+	
+	@RequestMapping(value="/adList.br")
+	public ModelAndView adList() {
+		ModelAndView mv = new ModelAndView("Ad/adList");
+		return mv;
+	}
+	@RequestMapping(value="/adDetail.br")
+	public ModelAndView adDetail() {
+		ModelAndView mv = new ModelAndView("Ad/adDetail");
+		return mv;
+	}
+	@RequestMapping(value="/adInfo.br")
+	public ModelAndView adInfo() {
+		ModelAndView mv = new ModelAndView("Ad/adInfo");
+		return mv;
+	}
+	@RequestMapping(value="/adComment.br")
+	public ModelAndView adComment() {
+		ModelAndView mv = new ModelAndView("Ad/adComment");
+		return mv;
+	}
 }
