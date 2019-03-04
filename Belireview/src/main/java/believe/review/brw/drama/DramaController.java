d2ecbfd5a72adbb622b40f05a53f49337be95796
rtg9502 <rtg9502@naver.com>
2019-03-04 10:26:09 +0900

    k

--- Belireview/src/main/java/believe/review/brw/drama/DramaController.java
+++ Belireview/src/main/java/believe/review/brw/drama/DramaController.java
@@ -1,5 +1,53 @@
 package believe.review.brw.drama;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+@RequestMapping("/drama")
 public class DramaController {
 
+   @RequestMapping(value="dramaList.br")
+   public ModelAndView dramaList() throws Exception{
+      
+      ModelAndView mv = new ModelAndView("/drama/dramaList");
+      
+      return mv;
+      
+   
+   }
+   
+   @RequestMapping(value="dramaDetail.br")
+   public ModelAndView dramaDetail() throws Exception{
+      
+      ModelAndView mv = new ModelAndView("/drama/dramaDetail");
+      
+      return mv;
+      
+   
+   }
+   
+   @RequestMapping(value="dramaInfo.br")
+   public ModelAndView dramaInfo() throws Exception{
+      
+      ModelAndView mv = new ModelAndView("/drama/dramaInfo");
+      
+      return mv;
+      
+   
+   }
+   
+   @RequestMapping(value="dramaComment.br")
+   public ModelAndView dramaComment() throws Exception{
+      
+      ModelAndView mv = new ModelAndView("/drama/dramaComment");
+      
+      return mv;
+      
+   
+   }
 }
--- Belireview/src/main/java/believe/review/brw/movie/MovieController.java
+++ Belireview/src/main/java/believe/review/brw/movie/MovieController.java
@@ -1,5 +1,38 @@
 package believe.review.brw.movie;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/movie")
 public class MovieController {
+   
+   @RequestMapping(value="/movieList")
+   public ModelAndView movieList() {
+      ModelAndView mv = new ModelAndView("movie/movieList");
+      return mv;
+   }
+   
+   @RequestMapping(value="/movieDetail")
+   public ModelAndView movieDetail() {
+      ModelAndView mv = new ModelAndView("movie/movieDetail");
+      return mv;
+   }
+   
+   @RequestMapping(value="/movieInfo")
+   public ModelAndView movieInfo() {
+      ModelAndView mv = new ModelAndView("movie/movieInfo");
+      return mv;
+   }
+   
+   @RequestMapping(value="/movieComment")
+   public ModelAndView movieComment() {
+      ModelAndView mv = new ModelAndView("movie/movieComment");
+      return mv;
+   }
+   
+   
+
 
 }
