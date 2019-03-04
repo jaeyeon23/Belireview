31d7bb07ab20063f9f9d3d353c0d17fd95654f79
toyaklya <toyaklya3@gmail.com>
2019-03-04 09:44:11 +0900

    asdf

--- Belireview/src/main/java/believe/review/brw/drama/DramaController.java
+++ Belireview/src/main/java/believe/review/brw/drama/DramaController.java
@@ -1,5 +1,53 @@
 package believe.review.brw.drama;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+@RequestMapping("/drama")
 public class DramaController {
 
+	@RequestMapping(value="dramaList.br")
+	public ModelAndView dramaList() throws Exception{
+		
+		ModelAndView mv = new ModelAndView("/Drama/dramaList");
+		
+		return mv;
+		
+	
+	}
+	
+	@RequestMapping(value="dramaDetail.br")
+	public ModelAndView dramaDetail() throws Exception{
+		
+		ModelAndView mv = new ModelAndView("/Drama/dramaDetail");
+		
+		return mv;
+		
+	
+	}
+	
+	@RequestMapping(value="dramaInfo.br")
+	public ModelAndView dramaInfo() throws Exception{
+		
+		ModelAndView mv = new ModelAndView("/Drama/dramaInfo");
+		
+		return mv;
+		
+	
+	}
+	
+	@RequestMapping(value="dramaComment.br")
+	public ModelAndView dramaComment() throws Exception{
+		
+		ModelAndView mv = new ModelAndView("/Drama/dramaComment");
+		
+		return mv;
+		
+	
+	}
 }
--- Belireview/src/main/java/believe/review/brw/movie/MovieController.java
+++ Belireview/src/main/java/believe/review/brw/movie/MovieController.java
@@ -2,4 +2,5 @@ package believe.review.brw.movie;
 
 public class MovieController {
 
+
 }
