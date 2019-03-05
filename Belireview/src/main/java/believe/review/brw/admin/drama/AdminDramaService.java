b9cf4733da71229761f255f2b1bd9d3dda0f09b6
jaeyeon23 <catblues282@gmail.com>
2019-03-05 17:38:27 +0900

    3.5 박재연 관리자 페이지 사용자 수정, 드라마 리스트 작성

--- Belireview/src/main/java/believe/review/brw/admin/drama/AdminDramaService.java
+++ Belireview/src/main/java/believe/review/brw/admin/drama/AdminDramaService.java
@@ -1,5 +1,11 @@
 package believe.review.brw.admin.drama;
 
+import java.util.List;
+import java.util.Map;
+
 public interface AdminDramaService {
 
+	public List<Map<String, Object>> selectDramaList() throws Exception;
+	
+	public List<Map<String, Object>> selectDramaList_order(Map<String, Object> map) throws Exception;
 }
--- Belireview/src/main/java/believe/review/brw/admin/drama/DramaController.java
+++
@@ -1,5 +0,0 @@
-package believe.review.brw.admin.drama;
-
-public class DramaController {
-
-}
