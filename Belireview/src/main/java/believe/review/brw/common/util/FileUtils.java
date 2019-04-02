6a22007c0a19041140e9d3d6549e125a7a36fdf3
juy0ung <dkzmnjer@gmail.com>
2019-04-02 10:32:09 +0900

    이건 아니에요!!!

--- Belireview/src/main/java/believe/review/brw/common/util/FileUtils.java
+++ Belireview/src/main/java/believe/review/brw/common/util/FileUtils.java
@@ -104,7 +104,7 @@ public class FileUtils {
 	//user용
 	//private static final String filePath_P = "C:\\인영\\sts\\Belireview\\Belireview\\src\\main\\webapp\\resources\\images\\user_profile\\";
 	//private static final String filePath_P = "C:\\Users\\박재연\\Desktop\\Belireview\\Belireview\\src\\main\\webapp\\resources\\images\\user_profile\\";
-	private String filePath_P = "C:\\Java\\pro\\Belireview\\src\\main\\webapp\\resources\\images\\user_profile\\";
+	private String filePath_P = "C:\\java\\App\\Belireview\\Belireview\\src\\main\\webapp\\resources\\images\\user_profile\\";
 	
 	public Map<String, Object> parseInsertFileInfo(Map<String, Object> map, HttpServletRequest request)
 			throws Exception {
